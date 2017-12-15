7452d6dc8634f86d27911b8113b5f776c18d68dc
Francis-PC\Francis <francis94c@gmail.com>
2017-12-15 13:27:55 +0100

    matcher.is_match unit tests

--- lib/pd_utility/matcher/test_matcher.py
+++ lib/pd_utility/matcher/test_matcher.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from unittest import TestCase
 import matcher
 
-
 __author__ = "Francis Ilechukwu"
 __project__ = "Python Dance"
 
@@ -10,3 +9,11 @@ __project__ = "Python Dance"
 class TestMatcher(TestCase):
     def test_is_match(self):
         self.assertTrue(matcher.is_match({"a": 1, "b": 2, "c": 3, "d": 4}, {"a": 1, "b": 2, "c": 3, "d": 4}))
+        self.assertFalse(matcher.is_match({"a": 1, "g": 2, "c": 3, "d": 4}, {"r": 1, "b": 2, "c": 3, "d": 4}))
+        self.assertFalse(matcher.is_match({"a": 1, "b": 3, "c": 3, "d": 4}, {"a": 1, "b": 2, "c": 3, "d": 4}))
+        self.assertFalse(matcher.is_match({"a": 1, "b": [5, 6, 7], "c": 3, "d": 4}, {"a": 1, "b": 2, "c": 3, "d": 4}))
+        self.assertTrue(matcher.is_match({"a": 1, "b": [5, 6, 7], "c": 3, "d": 4}, {"a": 1, "b": [5, 6, 7], "c": 3,
+                                                                                    "d": 4}))
+        self.assertFalse(matcher.is_match({"a": 1, "b": [5, 6, 7], "c": 3, "d": 4}, {"a": 1, "b": [7, 8, 9], "c": 3,
+                                                                                     "d": 4}))
+        self.assertTrue(matcher.is_match({"a": 1, "b": [5, 6, 7], "c": 3, "d": 4}, {"a": 1, "b": [7, 5, 6], "c": 3, "d": 4}))
