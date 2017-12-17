9e275e2c91e16945f7dfd2665c065393a0748b99
Francis-PC\Francis <francis94c@gmail.com>
2017-12-17 18:01:14 +0100

    Documentation and nme changes.

---
+++ dist/pd_utility/matcher/matcher.py
@@ -0,0 +1,15 @@
+def is_match(dict1, dict2):
+    for key, value in dict1.iteritems():
+        if key not in dict2:
+            return False
+        else:
+            if isinstance(value, dict) and isinstance(dict2[key], dict):
+                is_match(value, dict2[key])
+            elif isinstance(value, list) and isinstance(dict2[key], list):
+                if set(value) != set(dict2[key]):
+                        return False
+            else:
+                if value != dict2[key]:
+                    return False
+    return True
+
---
+++ dist/pd_utility/matcher/test_matcher.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+from unittest import TestCase
+import matcher
+
+__author__ = "Francis Ilechukwu"
+__project__ = "Python Dance"
+
+
+class TestMatcher(TestCase):
+    def test_is_match(self):
+        self.assertTrue(matcher.is_match({"a": 1, "b": 2, "c": 3, "d": 4}, {"a": 1, "b": 2, "c": 3, "d": 4}))
+        self.assertFalse(matcher.is_match({"a": 1, "g": 2, "c": 3, "d": 4}, {"r": 1, "b": 2, "c": 3, "d": 4}))
+        self.assertFalse(matcher.is_match({"a": 1, "b": 3, "c": 3, "d": 4}, {"a": 1, "b": 2, "c": 3, "d": 4}))
+        self.assertFalse(matcher.is_match({"a": 1, "b": [5, 6, 7], "c": 3, "d": 4}, {"a": 1, "b": 2, "c": 3, "d": 4}))
+        self.assertTrue(matcher.is_match({"a": 1, "b": [5, 6, 7], "c": 3, "d": 4}, {"a": 1, "b": [5, 6, 7], "c": 3,
+                                                                                    "d": 4}))
+        self.assertFalse(matcher.is_match({"a": 1, "b": [5, 6, 7], "c": 3, "d": 4}, {"a": 1, "b": [7, 8, 9], "c": 3,
+                                                                                     "d": 4}))
+        self.assertTrue(matcher.is_match({"a": 1, "b": [5, 6, 7], "c": 3, "d": 4}, {"a": 1, "b": [7, 5, 6], "c": 3,
+                                                                                    "d": 4}))
+        self.assertTrue(
+            matcher.is_match({"a": 1, "b": [5, 6, 7], "c": 3, "d": 4}, {"a": 1, "b": [7, 5, 6], "c": 3, "d": 4,
+                                                                        "e": 5}))
+        self.assertFalse(
+            matcher.is_match({"a": 1, "b": [5, 6, 7], "c": 3, "d": 4, "e": 5}, {"a": 1, "b": [7, 5, 6], "c": 3,
+                                                                                "d": 4}))
